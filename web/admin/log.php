eb7939bc6bdf8c15b1f87bcea22da38d27a6da97
Rakulan SIvathasan <s.rakulan04@gmail.com>
2024-06-03 11:14:14 +0200

    update log.php

--- web/admin/log.php
+++ web/admin/log.php
@@ -1,24 +1,27 @@
 <?php
-// Fonction pour enregistrer les journaux d'activité
-
-
+// Fonction pour obtenir l'adresse IP du client
 function getIPAddress() {
+    // Vérifier si une IP est passée par un proxy
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        // HTTP_X_FORWARDED_FOR peut contenir une liste d'IP (proxy chain)
+        $ip_list = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $ip = trim($ip_list[0]);
     } else {
+        // Sinon, prendre l'IP de l'utilisateur direct
         $ip = $_SERVER['REMOTE_ADDR'];
     }
     return $ip;
 }
 
-function logActivity($path,  $action, $user_id = null) {
-    // Vous pouvez personnaliser cette fonction pour enregistrer les journaux dans votre base de données ou dans un fichier de journal
+// Fonction pour enregistrer les journaux d'activité
+function logActivity($path, $action, $user_id = null) {
+    // Définir le fuseau horaire
     date_default_timezone_set('Europe/Paris');
     $timestamp = date("Y/m/d H:i:s");
 
-    if (isset($user) && !empty($user)) {
+    if (isset($user_id) && !empty($user_id)) {
         $user = '_'.$user_id;
         $id = $user_id;
     } elseif (isset($_SESSION['admin_id']) && !empty($_SESSION['admin_id'])) {
@@ -26,9 +29,15 @@ function logActivity($path,  $action, $user_id = null) {
         $id = $_SESSION["admin_id"];
     } else {
         $user = '';
+        $id = 'anonymous'; // Pour éviter les erreurs, on peut définir un ID par défaut
     }
 
+    // Obtenir l'adresse IP du client
     $ip_address = getIPAddress();
+
+    // Créer le log
     $log = "$ip_address - $timestamp - $id - $action\n";
+
+    // Enregistrer le log dans le fichier
     file_put_contents($path .'logs/activity_log'. $user .'.txt', $log, FILE_APPEND);
 }
